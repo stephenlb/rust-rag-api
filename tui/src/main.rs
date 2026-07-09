f1c2b3eef937107bbdfdc3baab67d72a21ab80bc
Stephen Blum <blum.stephen@gmail.com>
2026-07-09 09:25:44 -0700

    added TUI

---
+++ tui/src/main.rs
@@ -0,0 +1,125 @@
+use std::io;
+
+//use crossterm::style::Stylize;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::{Rect, Constraint},
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+
+#[derive(Debug, Default)]
+struct App {
+    history: Vec<String>,
+    prompt: String,
+    exit: bool,
+}
+
+fn main() -> io::Result<()> {
+    let mut app = App::default();
+    app.prompt = "type text here".to_string();
+    app.history.push("Hello, what do you want?".to_string());
+    app.history.push("Hello Laura Maxwell".to_string());
+    app.history.push("Hello Quickfix".to_string());
+    ratatui::run(|terminal| app.run(terminal))
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Esc => self.exit(),
+            //KeyCode::Left => self.decrement_counter(),
+            //KeyCode::Right => self.increment_counter(),
+            KeyCode::Backspace => {
+                self.prompt.pop();
+            },
+            KeyCode::Enter => {
+                self.history.push(self.prompt.to_string());
+                self.prompt.clear();
+            },
+            KeyCode::Char(c) => {
+                self.prompt.push(c);
+            },
+            _ => {},
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+}
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" PubNub Search ".bold());
+        let instructions = Line::from(vec![
+            " <Enter>".blue().bold(),
+            " Submit".into(),
+            " <Esc>".blue().bold(),
+            " Quit ".into(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            //.title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let height: i32 = area.height as i32;
+        let length: i32 = self.history.len() as i32;
+        let offset: i32 = 4;
+        let start: usize = *vec![
+            0, length - height + offset,
+        ].iter().max().unwrap() as usize;
+
+        let messages: Vec<Line> = self.history[start..length as usize]
+            .iter()
+            .map(|string| Line::from(string.to_string()))
+            .collect();
+
+        let chat_history = Text::from(messages).on_magenta().dim().bold();
+        Paragraph::new(chat_history)
+            .block(block)
+            .render(area, buf);
+
+        let prompt = Text::from(vec![Line::from(self.prompt.to_string())]);
+        let prompt_area = Rect {
+            x: area.x,
+            y: area.height - 3,
+            width: area.width,
+            height: area.height,
+        };
+
+        let prompt_block = Block::bordered()
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        Paragraph::new(prompt)
+            .block(prompt_block)
+            .render(prompt_area, buf);
+    }
+}
